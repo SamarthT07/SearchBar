26638b399bf6c3775638673cbfe00e4ed7b1a547
Samarth Tate <samarthtate07@gmail.com>
2022-10-30 22:11:57 +0530

    Addon

---
+++ main2.js
@@ -0,0 +1,17 @@
+const label = document.querySelector(".label");
+const line = document.querySelector(".line");
+const loox = document.querySelector(".loox");
+const looy = document.querySelector(".looy");
+const search = document.querySelector(".search");
+
+label.addEventListener("click" , ()=>{
+    search.classList.add("ac");
+    line.classList.add("ac");
+    loox.classList.add("ac");
+    looy.classList.add("ac");
+})
+
+
+
+
+
